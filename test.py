087784f0260902bcf2422b1f981058530ea7f619
Yvictor <fate2314@gmail.com>
2017-04-15 23:22:31 +0800

    Update test.py

--- test.py
+++ test.py
@@ -7,10 +7,11 @@ import sys
 
 print("Python Version: %s" % sys.version)
 
-display = Display(visible=0, size=(1024, 768))
+display = Display(visible=0, size=(1920, 720))
 display.start()
 
 driver = webdriver.Firefox()
 test_url = "http://twtraffic.tra.gov.tw/twrail/"
 driver.get(test_url)
 print("Done Selenium Work Well.")
+display.close()
